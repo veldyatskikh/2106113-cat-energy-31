25a851b142c3bdea42f5db6019677cf558ab4991
veldyatskikh <sveldyatskikh@gmail.com>
2024-06-05 18:28:44 +0300

    Вносит правки по стилизации всех страниц

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+
+/* мобильное меню*/
+document.querySelector('.main-nav__list').classList.remove('main-nav__list--open');
+document.querySelector('.main-nav__toggle').classList.remove('main-nav__toggle--close');
+const e = document.querySelector('.main-nav__list'),
+  n = document.querySelector('.main-nav__toggle');
+n.addEventListener('click', ()=>{
+  e.classList.toggle('main-nav__list--open');
+  n.classList.toggle('main-nav__toggle--close');
+}
+);
+
+/* слайдер*/
+
