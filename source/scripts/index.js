b16bd72418dc77bda07f5338d0b90daa7b4eab3b
veldyatskikh <sveldyatskikh@gmail.com>
2024-06-08 18:20:56 +0300

    Добавялет правки для защиты преокта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,12 @@
 /* в этот файл добавляет скрипты*/
 
 /* мобильное меню*/
-document.querySelector('.main-nav__list').classList.remove('main-nav__list--open');
-document.querySelector('.main-nav__toggle').classList.remove('main-nav__toggle--close');
+document.querySelector('.main-nav__list').classList.remove('main-nav__list--close');
+document.querySelector('.main-nav__toggle').classList.add('main-nav__toggle--close');
 const e = document.querySelector('.main-nav__list'),
   n = document.querySelector('.main-nav__toggle');
 n.addEventListener('click', ()=>{
-  e.classList.toggle('main-nav__list--open');
+  e.classList.toggle('main-nav__list--close');
   n.classList.toggle('main-nav__toggle--close');
 }
 );
