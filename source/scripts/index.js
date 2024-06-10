f6dff88e92240d706f834d09dd40b02e179b762a
veldyatskikh <sveldyatskikh@gmail.com>
2024-06-10 22:19:56 +0300

    Добавляет правки для защиты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,13 @@
-/* в этот файл добавляет скрипты*/
+/* в этот файл  добавляет скрипты*/
 
 /* мобильное меню*/
-document.querySelector('.main-nav__list').classList.remove('main-nav__list--close');
-document.querySelector('.main-nav__toggle').classList.add('main-nav__toggle--close');
+document.querySelector('.main-nav__list').classList.add('main-nav__list--close');
+document.querySelector('.main-nav__toggle').classList.add('main-nav__toggle--open');
 const e = document.querySelector('.main-nav__list'),
   n = document.querySelector('.main-nav__toggle');
 n.addEventListener('click', ()=>{
-  e.classList.toggle('main-nav__list--close');
+  e.classList.toggle('main-nav__list--open');
   n.classList.toggle('main-nav__toggle--close');
+
 }
 );
